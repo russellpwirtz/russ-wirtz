207ae0960f004e8034cf3380bdae3c055bf00de6
Russ Wirtz <russ@mail.com>
2023-03-29 10:12:06 -0700

    Restrict api key access

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY
-  },
   images: {
     domains: ['picsum.photos', 'maymont.org', 'news.clas.ufl.edu', 'www.lifeinnorway.net'],
   },
