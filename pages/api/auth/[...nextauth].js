6d63fb35df9536ed47d7ce3074f05cf345ad366a
Russ Wirtz <russ@mail.com>
2023-03-30 19:37:30 -0700

    secrets secrets are no fun

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,27 +1,24 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from "next-auth/providers/google";
 
-let clientId;
-let clientSecret;
 if (!process.env.GOOGLE_CLIENT_ID) {
   try {
-    clientId = JSON.parse(process.env.AMPLIFY_SECRETS).GOOGLE_CLIENT_ID;
-    clientSecret = JSON.parse(process.env.AMPLIFY_SECRETS).GOOGLE_CLIENT_SECRET;
+    process.env.GOOGLE_CLIENT_ID = JSON.parse(process.env.AMPLIFY_SECRETS).GOOGLE_CLIENT_ID;
+    process.env.GOOGLE_CLIENT_SECRET = JSON.parse(process.env.AMPLIFY_SECRETS).GOOGLE_CLIENT_SECRET;
+    process.env.NEXTAUTH_SECRET = JSON.parse(process.env.AMPLIFY_SECRETS).NEXTAUTH_SECRET;
+    console.error("Set next auth secret: " + process.env.NEXTAUTH_SECRET);
   } catch (error) {
     console.error("Error parsing api key: " + error.message);
     res.status(500).json({ error: 'An error occurred on the server.' });
     return;
   }
-} else {
-  clientId = process.env.GOOGLE_CLIENT_ID;
-  clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 }
 
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: clientId,
-      clientSecret: clientSecret,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
   ],
   secret: process.env.NEXTAUTH_SECRET,
