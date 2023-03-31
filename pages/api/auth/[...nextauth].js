dd831d0519890408e72ec86499c54c7d9d6867c4
Russ Wirtz <russ@mail.com>
2023-03-30 19:46:47 -0700

    seeeeecrets

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,25 +1,25 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from "next-auth/providers/google";
 
-if (!process.env.GOOGLE_CLIENT_ID) {
-  try {
-    process.env.GOOGLE_CLIENT_ID = JSON.parse(process.env.AMPLIFY_SECRETS).GOOGLE_CLIENT_ID;
-    process.env.GOOGLE_CLIENT_SECRET = JSON.parse(process.env.AMPLIFY_SECRETS).GOOGLE_CLIENT_SECRET;
-    process.env.NEXTAUTH_SECRET = JSON.parse(process.env.AMPLIFY_SECRETS).NEXTAUTH_SECRET;
-    console.error("Set next auth secret: " + process.env.NEXTAUTH_SECRET);
-  } catch (error) {
-    console.error("Error parsing api key: " + error.message);
-    res.status(500).json({ error: 'An error occurred on the server.' });
-    return;
-  }
-}
+// if (!process.env.GOOGLE_CLIENT_ID) {
+//   try {
+//     process.env.GOOGLE_CLIENT_ID = JSON.parse(process.env.AMPLIFY_SECRETS).GOOGLE_CLIENT_ID;
+//     process.env.GOOGLE_CLIENT_SECRET = JSON.parse(process.env.AMPLIFY_SECRETS).GOOGLE_CLIENT_SECRET;
+//     process.env.NEXTAUTH_SECRET = JSON.parse(process.env.AMPLIFY_SECRETS).NEXTAUTH_SECRET;
+//     console.error("Set next auth secret: " + process.env.NEXTAUTH_SECRET);
+//   } catch (error) {
+//     console.error("Error parsing api key: " + error.message);
+//     res.status(500).json({ error: 'An error occurred on the server.' });
+//     return;
+//   }
+// }
 
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: JSON.parse(process.env.AMPLIFY_SECRETS).GOOGLE_CLIENT_ID,
+      clientSecret: JSON.parse(process.env.AMPLIFY_SECRETS).GOOGLE_CLIENT_SECRET,
     })
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: JSON.parse(process.env.AMPLIFY_SECRETS).NEXTAUTH_SECRET,
 })
